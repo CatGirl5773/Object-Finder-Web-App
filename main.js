9cc2fe80a9b1f725b044e65cb400e1c47b3bb49a
Esther Choi <81777226+CatGirl5773@users.noreply.github.com>
2021-08-16 15:51:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 status = "";
 
 function setup() {
@@ -10,6 +11,27 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "") {
+        objectDetector.detect(video, gotResult);
+        for(var i = 0; i < objects.length; i++) {
+            percent = floor(objects[i].confidence * 100);
+            fill("#FF0000");
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label == value) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_found").innerHTML = "Object mentioned found";
+                synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance("Object mentioned found");
+                synth.speak(utterThis);
+            } else {
+                document.getElementById("object_found").innerHTML = "Object mentioned not found";
+            }
+        }
+    }
 }
 
 function start() {
@@ -21,4 +43,12 @@ function start() {
 function modelLoaded() {
     console.log("Model loaded!");
     status = true;
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
